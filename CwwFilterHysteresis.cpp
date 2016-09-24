e8b4c8c988f3f6beaa102bb9047e163a48094f87
Wolf Witt <robotobyte@gmail.com>
2016-09-24 14:32:25 -0700

    V1.00 Beta 01
    Original version, beta 1.

---
+++ CwwFilterHysteresis.cpp
@@ -0,0 +1,144 @@
+// ****************************************************************************
+//
+// Hysteresis Filter
+// -----------------
+// Code by W. Witt; V1.00-beta-01; June 2016
+//
+// This code implemented a configurable hysteresis filter.
+//
+// ****************************************************************************
+
+#include <limits.h>
+#include <Arduino.h>
+
+#include "CwwFilterHysteresis.h"
+
+// ============================================================================
+// Constructors, Destructor
+// ============================================================================
+
+CwwFilterHysteresis::CwwFilterHysteresis ( uint8_t thresholdCount ) {
+
+  if ( thresholdCount < 1 ) thresholdCount = 1;
+  this->thresholdCount = thresholdCount;
+
+  thresholdList = (structThresholdPoint *)
+                  malloc ( thresholdCount * sizeof(structThresholdPoint) );
+
+  lastValue = 0;
+
+}
+
+// ----------------------------------------------------------------------------
+
+CwwFilterHysteresis::~CwwFilterHysteresis () {
+
+  free ( thresholdList );
+
+}
+
+// ============================================================================
+// Public Functions
+// ============================================================================
+
+boolean CwwFilterHysteresis::defineThreshold (
+  uint8_t           thresholdIndex,
+  CWW_FH_POINT_TYPE thPointCenter,
+  CWW_FH_POINT_TYPE offsetAroundCenter
+) {
+
+  return defineThreshold (
+    thresholdIndex,
+    thPointCenter - offsetAroundCenter,
+    thPointCenter,
+    thPointCenter + offsetAroundCenter
+  );
+
+}
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+boolean CwwFilterHysteresis::defineThreshold (
+  uint8_t           thresholdIndex,
+  CWW_FH_POINT_TYPE thPointLow,
+  CWW_FH_POINT_TYPE thPointCenter,
+  CWW_FH_POINT_TYPE thPointHigh
+) {
+
+  boolean pointIsValid;
+
+  pointIsValid = thresholdIndex < thresholdCount;
+
+  if ( pointIsValid ) {
+    thresholdList[ thresholdIndex ].high   = thPointHigh;
+    thresholdList[ thresholdIndex ].center = thPointCenter;
+    thresholdList[ thresholdIndex ].low    = thPointLow;
+   }
+
+  return pointIsValid;
+
+}
+
+// ----------------------------------------------------------------------------
+
+void CwwFilterHysteresis::initHistory ( CWW_FH_POINT_TYPE initValue ) {
+  
+  uint8_t thIndex;
+  boolean zoneMatch;
+
+  thIndex = 0;
+  zoneMatch = false;
+  do {
+    zoneMatch = initValue < thresholdList[thIndex].center;
+    if ( ! zoneMatch ) thIndex++;
+  } while ( ! zoneMatch && thIndex < thresholdCount );
+
+  lastValue = initValue;
+  lastZone  = thIndex;  
+
+}
+
+// ----------------------------------------------------------------------------
+
+uint8_t CwwFilterHysteresis::mapValueToZone ( CWW_FH_POINT_TYPE value ) {
+
+  uint8_t           thIndex;
+  CWW_FH_POINT_TYPE threshold;
+  boolean           zoneMatch;
+
+//if ( value == lastValue ) return lastZone;
+
+  thIndex = 0;
+  zoneMatch = false;
+  do {
+    threshold = thIndex < lastZone 
+                ? thresholdList[thIndex].low
+                : thresholdList[thIndex].high;
+    zoneMatch = lastValue < threshold;
+    if ( ! zoneMatch ) thIndex++;
+  } while ( ! zoneMatch && thIndex < thresholdCount );
+
+  lastValue = value;
+  lastZone  = thIndex;
+
+  return thIndex;
+
+}
+
+// ----------------------------------------------------------------------------
+
+CWW_FH_POINT_TYPE CwwFilterHysteresis::lastMappedValue () {
+
+  return lastValue;
+
+}
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+uint8_t CwwFilterHysteresis::lastMappedZone () {
+
+  return lastZone;
+
+}
+
+// ****************************************************************************
---
+++ CwwFilterHysteresis.h
@@ -0,0 +1,119 @@
+// *****************************************************************************
+//
+// Hysteresis Filter
+// -----------------
+// Code by W. Witt; V1.00-beta-01; June 2016
+//
+// This code defines a configurable hysteresis filter.
+//
+// Each filter instance may be configured with a different number of
+// thresholds, but the threshold count must be at least one. n thresholds
+// create m=n+1 zones, and the filter then compares input values to
+// thresholds and accordingly maps each input value to a zone.
+//
+// Each threshold must be given three values: low, center, high.
+// The filter requires that:
+// 1) threshold 0 center < threeshold 1 center, etc.
+// 2) for each threshold, low < center < high
+//
+// The filter then maps input values to zones as follows:
+//
+//           threshold 0 low                 threshold 1 low
+//           |   threshold 0 center          |     threshold 1 center
+//           |   |   threshold 0 high        |     |     threshold 1 high
+//           |   |   |                       |     |     |
+//           v   v   v                       v     v     v
+//  
+//           .   |   .                       .     |     .
+//           .   |   .                       .     |     .
+//           .   |   .                       .     |     .
+//    <==================================================================>
+//   low                           Input Value                         high
+//               |                                 |
+//    <<-------->|<------------------------------->|<------------------->>
+//       zone 0                zone 1                       zone 2
+//
+//       ------------|-----------------------------------|------------>
+//       increasing input values are checked against high thresholds
+//       to transition from zone m to m+1, the high threshold must be crossed
+//
+//       <---|-------------------------------|-------------------------
+//           decreasing input values are checked against low thresholds
+//           from zone m to m-1, the low threshold must be crossed
+//
+//       center thresholds are used for during initialization
+//
+// *****************************************************************************
+
+#ifndef CwwFilterHysteresis_h
+#define CwwFilterHysteresis_h
+
+// *****************************************************************************
+
+#include <Arduino.h>
+
+// =============================================================================
+
+// Define type of values that will be input to the filter...
+#define CWW_FH_POINT_TYPE float
+
+// ----------------------------------------------------------------------------
+
+class CwwFilterHysteresis {
+
+  public:
+
+    // Public Types:
+
+    struct structThresholdPoint {
+      CWW_FH_POINT_TYPE high;
+      CWW_FH_POINT_TYPE center;
+      CWW_FH_POINT_TYPE low;
+    };
+
+    // Public Constants:
+
+    // Public Variables:
+
+    // Public Functions:
+
+             CwwFilterHysteresis ( uint8_t thresholdCount );
+    virtual ~CwwFilterHysteresis ();
+
+    boolean defineThreshold ( uint8_t           thresholdIndex,
+                              CWW_FH_POINT_TYPE thPointCenter,
+                              CWW_FH_POINT_TYPE offsetAroundCenter
+                            );
+    boolean defineThreshold ( uint8_t           thresholdIndex,
+                              CWW_FH_POINT_TYPE thPointLow,
+                              CWW_FH_POINT_TYPE thPointCenter,
+                              CWW_FH_POINT_TYPE thPointHigh
+                            );
+
+    void    initHistory    ( CWW_FH_POINT_TYPE initValue );
+    uint8_t mapValueToZone ( CWW_FH_POINT_TYPE value     );
+
+    CWW_FH_POINT_TYPE lastMappedValue ();
+    uint8_t           lastMappedZone  ();
+
+  private:
+
+    // Private Constants:
+
+    // Private Variables:
+
+    structThresholdPoint * thresholdList;
+    uint8_t                thresholdCount;
+
+    CWW_FH_POINT_TYPE lastValue;
+    uint8_t           lastZone;
+
+    // Private Functions:
+
+};
+
+// *****************************************************************************
+
+#endif
+
+// *****************************************************************************
